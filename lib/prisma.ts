7322038659234ea30050713f1e31e83b55363c8b
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-03-06 23:37:46 -0500

    Add adapter for sqlite

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,17 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"]
   });
 
