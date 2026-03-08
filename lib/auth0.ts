48c05c342507cd0c70a90814eaf1ad20ebb29438
Jasraj Johal <rsvlt.04.coder@gmail.com>
2026-03-07 20:53:24 -0500

    fix: Correct typo in authorizationParameters in Auth0Client configuration

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,7 +1,7 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
 export const auth0 = new Auth0Client({
-  authorizationParams: {
+  authorizationParameters: {
     audience: process.env.AUTH0_AUDIENCE,
   },
   routes: {
